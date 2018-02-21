cbc3f719d92fc21e33f7d312de68505e99a10c8a
David Arelano <dtarellano1@gmail.com>
2018-02-20 20:12:48 -0800

    solved find duplicates problem

---
+++ problems/containsDuplicates.js
@@ -0,0 +1,26 @@
+// Given an array of integers, find if the array contains any duplicates. Your
+// function should return true if any value appears at least twice in the
+// array, and it should return false if every element is distinct.
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+	let store = {};
+
+	if (!nums.length) {
+		return false;
+	}
+	for (let num of nums) {
+		if (!store[num]) {
+			store[num] = 1;
+		} else {
+			store[num]++;
+		}
+		if (store[num] > 1) {
+			return true;
+		}
+	}
+	return false;
+};
